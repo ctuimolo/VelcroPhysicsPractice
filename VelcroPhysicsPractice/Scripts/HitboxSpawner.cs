041e26740ef841688e4dc37d14b56869aeeea544
unknown <tuimoloau@gmail.com>
2019-01-22 17:02:23 -0800

    Started HitboxSpawner...

--- VelcroPhysicsPractice/Scripts/HitboxSpawner.cs
+++ VelcroPhysicsPractice/Scripts/HitboxSpawner.cs
@@ -11,13 +11,32 @@ using System.Collections.Generic;
 
 namespace VelcroPhysicsPractice.Scripts
 {
-    class HitboxSpawner
+    struct TimedHitbox
     {
+        Hitbox hitbox;
+        short timer;
+        int id;
+    }
+
+    struct StaticHitbox
+    {
+        Hitbox hitbox;
+        int id;
+    }
+
+    public class HitboxSpawner
+    {
+        private List<Hitbox> timeHitboxList;
+        private Dictionary<Hitbox, int> hitboxDictionary;
+
         HitboxSpawner()
         {
 
         }
 
-
+        public void CreateTimeHitbox(int setTimer, World rootWorld, SpriteBatch rootSpriteBatch, ContentManager rootContent, GameObject setOwner, Rectangle coordinates, string color, string setValue = "")
+        {
+            
+        }
     }
 }
